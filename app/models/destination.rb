fb4b46b22a932c9a3c53d8c279558a7dae3fb073
Pasikonik <blind.x6@gmail.com>
2016-03-27 20:49:05 +0200

    update destination model

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,4 +1,20 @@
 class Destination < ApplicationRecord
   has_many :items
   belongs_to :user
+
+  def queue
+    items.where(status: 'wait').reverse
+  end
+
+  def sent_list
+    items.where(status: 'sent')
+  end
+
+  def get_last_from_queue
+    queue.last
+  end
+
+  def page_token
+    data = user.get_page_token(fb_page)
+  end
 end
