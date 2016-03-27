343f3f20f5bef9d8968c9d3e3816648613ab8f8a
Pasikonik <blind.x6@gmail.com>
2016-03-27 20:55:56 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  root to: 'application#index'
-  get '/' => 'application#index'
+  # devise_scope :user do
+  #   get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+  #   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
+
+  root to: 'home#index'
 
   resources :destinations
+  resources :items
 end
